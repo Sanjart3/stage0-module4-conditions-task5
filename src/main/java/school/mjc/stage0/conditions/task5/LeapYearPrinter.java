01b4be861059cb332f1afbd602641ab877bef13a
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-26 12:28:49 +0500

    just

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        year%=4;
+        switch (year){
+            case 0:
+                System.out.println("leap"); break;
+            default:
+                System.out.println("not leap");
+        }
     }
 }
